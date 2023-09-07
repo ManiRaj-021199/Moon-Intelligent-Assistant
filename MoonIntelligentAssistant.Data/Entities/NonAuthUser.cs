9363500d2f0e3d6878ec27b1eac1a45543c8f6b4
Mani Raj <maniraj.021199@gmail.com>
2023-09-07 23:04:24 +0200

    ADDED: IsAuthenticated column added in NonAuthUser table

--- MoonIntelligentAssistant.Data/Entities/NonAuthUser.cs
+++ MoonIntelligentAssistant.Data/Entities/NonAuthUser.cs
@@ -13,5 +13,7 @@ public partial class NonAuthUser
 
     public string AuthCode { get; set; } = null!;
 
+    public bool IsAuthenticated { get; set; }
+
     public DateTime RegisterDate { get; set; }
 }
